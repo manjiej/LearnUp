8c1bdd6b8b43d79ff783f3c4e2b9483c7839b931
Jiang Man jie <55577171+manjiej@users.noreply.github.com>
2019-12-02 19:26:14 +0800

    changed schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,6 @@ ActiveRecord::Schema.define(version: 2019_12_02_063556) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "subjects", force: :cascade do |t|
-    t.string "name"
-    t.string "category"
-    t.integer "number_of_learners"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
