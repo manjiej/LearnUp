8954a3b87c311b6ffa5c1172498b8f1fad399cb7
Cecile WANG cecile_wang_95@hotmail.fr <cecile_wang_95@hotmail.fr>
2019-12-03 14:45:32 +0800

    Changed navbar style when login

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_03_030257) do
+ActiveRecord::Schema.define(version: 2019_12_03_030504) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "organizer"
+    t.date "timeline"
+    t.string "location"
+    t.integer "number_of_members"
+    t.bigint "user_id", null: false
+    t.bigint "subject_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["subject_id"], name: "index_groups_on_subject_id"
+    t.index ["user_id"], name: "index_groups_on_user_id"
+  end
 
   create_table "subjects", force: :cascade do |t|
     t.string "name"
@@ -56,9 +71,13 @@ ActiveRecord::Schema.define(version: 2019_12_03_030257) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "photo"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "groups", "subjects"
+  add_foreign_key "groups", "users"
   add_foreign_key "taggings", "tags"
 end
