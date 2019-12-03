76bfe764a87e2c7c45b894b3b7e35a054783ece1
Cecile WANG cecile_wang_95@hotmail.fr <cecile_wang_95@hotmail.fr>
2019-12-03 10:39:49 +0800

    Changed database (seed)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Cleaning all database.."
+Subject.destroy_all
+
+puts "Creating database..."
+
 Subject.create(name: "Mathematics", category: "Sciences", number_of_learners: "4")
 Subject.create(name: "English", category: "Language", number_of_learners: "6")
-Subject.create(name: "Mathematics", category: "Sciences", number_of_learners: "4")
-Subject.create(name: "English", category: "Language", number_of_learners: "6")
-Subject.create(name: "Mathematics", category: "Sciences", number_of_learners: "4")
-Subject.create(name: "English", category: "Language", number_of_learners: "6")
+Subject.create(name: "Algorithms", category: "IT", number_of_learners: "4")
+Subject.create(name: "Chinese", category: "Language", number_of_learners: "6")
+Subject.create(name: "Philosophy", category: "Arts & History", number_of_learners: "4")
+Subject.create(name: "Education", category: "Social Sciences", number_of_learners: "6")
+
 
