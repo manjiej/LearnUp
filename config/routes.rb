733706c5be1c9b1601206af707842e10e2ed2e72
Jiang Man jie <55577171+manjiej@users.noreply.github.com>
2019-12-04 10:47:21 +0800

    fixed conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  resources :subjects, only: [:index, :show, :new] do
-=======
   resources :subjects, only: [:new] do
->>>>>>> b880cf59212395347e5d4560edc3ed7a2d3cd466
     resources :groups, shallow: true do
       resources :comments, shallow: true
     end
