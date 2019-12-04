a9f681426aa1e5de37bea37accbcfccaf773bc2c
Yu Bai <white_by@126.com>
2019-12-04 14:36:42 +0800

    form for comments

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,6 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   # GET /groups
   # GET /groups.json
