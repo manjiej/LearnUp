156e0063bfcf13be26c9c783425936f814d0f064
Yu Bai <white_by@126.com>
2019-12-04 12:15:05 +0800

    push

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :groups]
+  skip_before_action :authenticate_user!, only: [:home, :group]
 
   def home
     @subjects = Subject.all
   end
+
+  def group
+    @groups = Group.where(subject_id: params[:subject_id])
+  end
+
+
 end
