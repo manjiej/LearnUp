085a2365fffe8682df852cf98f69a20c0a120b4e
Cecile WANG cecile_wang_95@hotmail.fr <cecile_wang_95@hotmail.fr>
2019-12-05 18:51:59 +0800

    delete migration

--- app/models/group.rb
+++ app/models/group.rb
@@ -11,10 +11,4 @@ class Group < ApplicationRecord
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
-<<<<<<< HEAD
-=======
-
-  has_many :group_members
-  has_many :members, through: :group_members, source: :user
->>>>>>> 2b840d993bff718b35802879aaac343b60ea5acf
 end
--- db/migrate/20191205072054_remove_number_of_members_from_groups.rb
+++
@@ -1,5 +0,0 @@
-class RemoveNumberOfMembersFromGroups < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :groups, :number_of_members, :integer
-  end
-end
